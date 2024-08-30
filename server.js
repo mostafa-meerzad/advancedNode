4a115637ab7f9326d859585ee8b2753ffcc5d2d9
mostafa <mostafameerzad@gmail.com>
2024-08-30 16:40:20 +0430

    connect to db

--- server.js
+++ server.js
@@ -1,23 +1,63 @@
-'use strict';
-require('dotenv').config();
-const express = require('express');
-const myDB = require('./connection');
-const fccTesting = require('./freeCodeCamp/fcctesting.js');
+"use strict";
+require("dotenv").config();
+const express = require("express");
+const myDB = require("./connection");
+const fccTesting = require("./freeCodeCamp/fcctesting.js");
+const session = require("express-session");
+const passport = require("passport");
+const mongodb = require("mongodb");
+const ObjectID = mongodb.ObjectID;
 
 const app = express();
 app.set("view engine", "pug");
 app.set("views", "./views/pug");
 
 fccTesting(app); //For FCC testing purposes
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use("/public", express.static(process.cwd() + "/public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: true,
+    saveUninitialized: true,
+    cookie: { secure: false },
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
-app.route('/').get((req, res) => {
-res.render("index", {title: "Hello", message: "Please log in"});
+myDB(async (client) => {
+  const myDataBase = await client.db("database").collection("users");
+
+  // Be sure to change the title
+  app.route("/").get((req, res) => {
+    // Change the response to render the Pug template
+    res.render("index", {
+      title: "Connected to Database",
+      message: "Please login",
+    });
+  });
+
+  // Serialization and deserialization here...
+
+  passport.serializeUser((user, done) => {
+    done(null, user._id);
+  });
+
+  passport.deserializeUser((id, done) => {
+    myDataBase.findOne({ _id: new ObjectID(id) }, (err, doc) => {
+      done(null, doc);
+    });
+  });
+  // Be sure to add this...
+}).catch((e) => {
+  app.route("/").get((req, res) => {
+    res.render("index", { title: e, message: "Unable to connect to database" });
+  });
 });
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log('Listening on port ' + PORT);
+  console.log("Listening on port " + PORT);
 });
