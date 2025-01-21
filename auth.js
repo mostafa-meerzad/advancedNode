d7e6c5c315e8e65fb428df9307b7c53bd4412061
mostafa <mostafameerzad@gmail.com>
2025-01-21 12:26:42 +0430

    add GitHubStrategy to the project

--- auth.js
+++ auth.js
@@ -1,15 +1,27 @@
 const passport = require("passport");
 const bcrypt = require("bcrypt");
 const LocalStrategy = require("passport-local");
+const GitHubStrategy = require("passport-github").Strategy;
+require("dotenv").config();
 
 module.exports = function (app, myDataBase) {
- passport.use(
+  // Serialization and deserialization here...
+  passport.serializeUser((user, done) => {
+    done(null, user._id);
+  });
+  passport.deserializeUser((id, done) => {
+    myDataBase.findOne({ _id: new ObjectID(id) }, (err, doc) => {
+      done(null, doc);
+    });
+  });
+
+  passport.use(
     new LocalStrategy((username, password, done) => {
       myDataBase.findOne({ username: username }, (err, user) => {
         console.log(`User ${username} attempted to log in.`);
         if (err) return done(err);
         if (!user) return done(null, false);
-        if (!bcrypt.compareSync(password, user.password)) { 
+        if (!bcrypt.compareSync(password, user.password)) {
           return done(null, false);
         }
         return done(null, user);
@@ -17,17 +29,18 @@ module.exports = function (app, myDataBase) {
     })
   );
 
-  
-    passport.serializeUser((user, done) => {
-      done(null, user._id);
-    });
-
-     // Serialization and deserialization here...
-
-  passport.deserializeUser((id, done) => {
-    myDataBase.findOne({ _id: new ObjectID(id) }, (err, doc) => {
-      done(null, doc);
-    });
-  });
-  
-}
+  passport.use(
+    new GitHubStrategy(
+      {
+        clientID: process.env.GITHUB_CLIENT_ID,
+        clientSecret: process.env.GITHUB_CLIENT_SECRET,
+        callbackURL:
+          "https://boilerplate-advancednode.sky020.repl.co/auth/github/callback",
+      },
+      function (accessToken, refreshToken, profile, cb) {
+        console.log(profile);
+        //Database logic here with callback containing your user object
+      }
+    )
+  );
+};
