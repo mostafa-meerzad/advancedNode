ff96b7cfc10aeccea59c54eb119a3cb011c7c067
mostafa <mostafameerzad@gmail.com>
2025-01-21 14:05:01 +0430

    count the users logged in

--- server.js
+++ server.js
@@ -6,12 +6,14 @@ const fccTesting = require("./freeCodeCamp/fcctesting.js");
 const session = require("express-session");
 const passport = require("passport");
 
-const routes = require("./routes.js")
-const auth = require("./auth.js")
+const routes = require("./routes.js");
+const auth = require("./auth.js");
 const app = express();
+const http = require("http").createServer(app);
+const io = require("socket.io")(http);
+
 app.set("view engine", "pug");
 app.set("views", "./views/pug");
-
 fccTesting(app); //For FCC testing purposes
 app.use("/public", express.static(process.cwd() + "/public"));
 app.use(express.json());
@@ -29,20 +31,20 @@ app.use(passport.session());
 
 myDB(async (client) => {
   const myDataBase = await client.db("database").collection("users");
+  let currentUsers = 0;
+  io.on("connection", (socket) => {
+    ++currentUsers;
+    io.emit('user count', currentUsers);
+    console.log("A user has connected");
+  });
 
   routes(app, myDataBase);
   auth(app, myDataBase);
- 
-
-
-  
 
   app.use((req, res, next) => {
-    res.status(404)
-      .type('text')
-      .send('Not Found');
+    res.status(404).type("text").send("Not Found");
   });
- 
+
   // Be sure to add this...
 }).catch((e) => {
   app.route("/").get((req, res) => {
@@ -51,8 +53,6 @@ myDB(async (client) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+http.listen(PORT, () => {
   console.log("Listening on port " + PORT);
 });
-
-
